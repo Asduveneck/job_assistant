343bc297c8d1f477f00ad0737a5d83562219a02b
Alex Duveneck <alexander.duveneck@gmail.com>
2020-05-02 00:45:03 -0700

    Set up linters

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins: ['@typescript-eslint', 'eslint-comments', 'simple-import-sort'],
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:eslint-comments/recommended',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'simple-import-sort/sort': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    // should turn on at some point
+    //      'import/prefer-default-export': 'off',
+    //      'import/no-default-export': 'error',
+    'react/jsx-props-no-spreading': 0, // should turn on at some point
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'no-use-before-define': ['error', {
+      functions: false,
+      classes: true,
+      variables: true
+    }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true
+      },
+    ],
+    'unicorn/prevent-abbreviations': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
