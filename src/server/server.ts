d74e11e33d9c9703ded2b9cdf87887a9dbfc7699
Alex Duveneck <alexander.duveneck@gmail.com>
2020-06-18 22:48:58 -0700

    update port to 5000 for server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
+import { createConnection } from 'typeorm';
+
 import 'dotenv/config';
 import apiRouter from './routes';
+import config from '../../ormconfig.json';
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(apiRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
