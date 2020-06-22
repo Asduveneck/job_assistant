d02949c6f4babd9b47d26d3c1559f225be71ed32
Alex Duveneck <alexander.duveneck@gmail.com>
2020-06-22 16:51:50 -0700

    update script and new config to listen to .env files

---
+++ ormconfig.ts
@@ -0,0 +1,23 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
+  migrationsRun: true,
+  logging: false,
+  entities: ['src/db/entity/**/*.js'],
+  migrations: ['src/db/migration/**/*.js'],
+  subscribers: ['src/db/subscriber/**/*.js'],
+  cli: {
+    entitiesDir: 'src/db/entity',
+    migrationsDir: 'src/db/migration',
+    subscribersDir: 'src/db/subscriber',
+  },
+};
+
+export = config;
