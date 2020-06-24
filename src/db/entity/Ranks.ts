af7f7c162a9b629d9d4a080ef4c4d7bf91652382
Alex Duveneck <alexander.duveneck@gmail.com>
2020-06-24 01:00:00 -0700

    add exports

--- src/db/entity/Ranks.ts
+++ src/db/entity/Ranks.ts
@@ -3,37 +3,37 @@ import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { FaveAvoidCompany, FaveAvoidJob, FaveAvoidPerson } from './helpers';
 
 @Entity({ orderBy: { rank: 'ASC' } })
-class FavoriteCompany extends FaveAvoidCompany {
+export class FavoriteCompany extends FaveAvoidCompany {
   @PrimaryGeneratedColumn()
   favCompanyId: number;
 }
 
 @Entity({ orderBy: { rank: 'ASC' } })
-class AvoidCompany extends FaveAvoidCompany {
+export class AvoidCompany extends FaveAvoidCompany {
   @PrimaryGeneratedColumn()
   avoidCompanyId: number;
 }
 
 @Entity({ orderBy: { rank: 'ASC' } })
-class FavoriteJob extends FaveAvoidJob {
+export class FavoriteJob extends FaveAvoidJob {
   @PrimaryGeneratedColumn()
   favJobId: number;
 }
 
 @Entity({ orderBy: { rank: 'ASC' } })
-class InterestedJob extends FaveAvoidJob {
+export class InterestedJob extends FaveAvoidJob {
   @PrimaryGeneratedColumn()
   interestedJobId: Number;
 }
 
 @Entity({ orderBy: { rank: 'ASC' } })
-class FavoritePerson extends FaveAvoidPerson {
+export class FavoritePerson extends FaveAvoidPerson {
   @PrimaryGeneratedColumn()
   favPersonId: number;
 }
 
 @Entity({ orderBy: { rank: 'ASC' } })
-class AvoidPerson extends FaveAvoidPerson {
+export class AvoidPerson extends FaveAvoidPerson {
   @PrimaryGeneratedColumn()
   avoidPersonId: number;
 }
