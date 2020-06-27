02b3b37dd9745b4aff384515dbf9e47bbb9ffb2a
Alex Duveneck <alexander.duveneck@gmail.com>
2020-06-27 01:05:00 -0700

    consolidate tsconfig and connects to one for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
-    // project: './tsconfig.json', // BUG: https://stackoverflow.com/questions/37579969/how-to-use-multiple-tsconfig-files-in-vs-code
+    project: './tsconfig.json', // bug? https://stackoverflow.com/questions/37579969/how-to-use-multiple-tsconfig-files-in-vs-code
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
     ecmaFeatures: {
