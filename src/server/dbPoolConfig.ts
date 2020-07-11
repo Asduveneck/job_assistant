566112e14ea43cdc2b398e9041b773d06e2e6957
Alex Duveneck <alexander.duveneck@gmail.com>
2020-07-11 00:45:29 -0700

    add initialization script for SQL
    initial bash and shell scripts to log and create table within db
    
    Create Person and Job db; update Company DB; list remaining tables
    
    wrap up tables profile, setting, template, note, task, progress, first_interview
    
    Identify issue with reference ID and filepaths
    
    Write script to drop tables for further polishing; update main script to dry up potential db interactions via bash
    
    update scripts and rename files to setup initialization
    
    update for joins and rank tables
    
    cleanup; fix ranks, alter, and updating helper scripts
    
    rename s to scripts for clarity
    
    add command to connect to psql within docker
    
    rename scripts to s; add help line for db; rename db_migrations to helper because more general now
    
    remove commented out code

--- src/server/dbPoolConfig.ts
+++ src/server/dbPoolConfig.ts
@@ -3,7 +3,6 @@ import { Pool } from 'pg';
 
 // const isTsNode = process[Symbol.for('ts-node.register.instance')]; // implicit type any
 
-// Refactor / Clean up below or check. Before PG was POSTGRES_
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   port: parseInt(process.env.POSTGRES_PORT),
@@ -14,24 +13,6 @@ const pool = new Pool({
   connectionTimeoutMillis: 0,
   idleTimeoutMillis: 0,
 });
-
-// Will probably initialize the tables in an init.sql file...
-// const query = `
-// CREATE TABLE company (
-//     company_id serial primary key,
-//     name varchar,
-//     career_url varchar,
-//     home_url varchar,
-//     email_pattern varchar,
-//     details json,
-//     other_urls json
-// );
-// `;
-
-/*
-
-
-*/
 
 export const query = (
   text: string,
