4fcd3691b8b143fd571501943b898814c2c32d1a
Alex Duveneck <alexander.duveneck@gmail.com>
2020-07-26 20:01:07 -0700

    Join tables to split the detail and history tables previously defined

---
+++ s/db_SQL/init-04_details_joins.sql
@@ -0,0 +1,44 @@
+\c job_assistant
+
+CREATE TABLE IF NOT EXISTS detail_to_person (
+  detail_id integer PRIMARY KEY,
+  person_id integer REFERENCES person (person_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
+CREATE TABLE IF NOT EXISTS detail_to_company (
+  detail_id integer PRIMARY KEY,
+  company_id integer REFERENCES company (company_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
+CREATE TABLE IF NOT EXISTS detail_to_job (
+  detail_id integer PRIMARY KEY,
+  job_id integer REFERENCES job (job_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
+CREATE TABLE IF NOT EXISTS detail_to_progress (
+  detail_id integer PRIMARY KEY,
+  progress_id integer REFERENCES progress (progress_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
+CREATE TABLE IF NOT EXISTS detail_to_task (
+  detail_id integer PRIMARY KEY,
+  task_id integer REFERENCES task (task_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
+CREATE TABLE IF NOT EXISTS detail_to_contact_history (
+  detail_id integer PRIMARY KEY,
+  contact_history_id integer REFERENCES contact_history (contact_history_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
+CREATE TABLE IF NOT EXISTS detail_to_interview_history (
+  detail_id integer PRIMARY KEY,
+  interview_history_id integer REFERENCES interview_history (interview_history_id),
+  FOREIGN KEY detail_id REFERENCES detail (detail_id)
+);
+
